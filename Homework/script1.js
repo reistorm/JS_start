983ac719f8eca9872945fa69d5213eb52fbaeffb
reistorm <rysaeva96@mail.ru>
2024-06-24 22:53:25 +0500

    init

---
+++ Homework/script1.js
@@ -0,0 +1,11 @@
+// Задание 1 
+
+const temperaturaCelsius = 30;
+let temperaturaFahrenheit = (9/5) * temperaturaCelsius + 32;
+console.log(temperaturaFahrenheit);
+
+// Задание 2
+
+const firstName = 'Lizi';
+let admin = firstName;
+console.log(admin);
---
+++ Seminars/script.js
@@ -0,0 +1,47 @@
+// задание 1
+console.log("Текст из script.js");
+// задание 2
+const num = 123;
+console.log("num = " + num);
+
+let a = 5;
+a = 7;
+console.log("a = " + a);
+
+const b = 1 + 2 + 3;
+console.log(`b = " + ${b}`);
+
+// задание 3
+const c = 10;
+const d = 2;
+
+let sumNumber = c + d;
+let diffNumber = c - d;
+let multiplicationNumber = c * d;
+let divisionNumber = c / d;
+console.log(`sumNumber = ${sumNumber}, diffNumber = ${diffNumber}, multiplicationNumber = ${multiplicationNumber}, divisionNumber = ${divisionNumber}`);
+let result = c + d + sumNumber + diffNumber + multiplicationNumber + divisionNumber;
+console.log(result);
+
+// Задание 5
+
+const e = 1.5;
+const h = 0.75;
+let sumNum = e + h;
+console.log(sumNum);
+
+let r = 45;
+console.log((-1) * r);
+
+// Задание 5
+
+let firstName = "Элиза";
+let lastName = "Рысаева";
+console.log(`${firstName} ${lastName}`);
+let java = "Java";
+let script = "script";
+console.log(java + script);
+
+let hello = "hello";
+let world = "world";
+console.log(hello + ' ' + world);
