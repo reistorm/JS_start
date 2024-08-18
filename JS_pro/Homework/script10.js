f2b95bd56c6c3848796ee0422daa7e8aa33c582f
reistorm <rysaeva96@mail.ru>
2024-08-18 23:07:06 +0500

    sc10

--- JS_pro/Homework/script10.js
+++ JS_pro/Homework/script10.js
@@ -4,24 +4,22 @@ body2.appendChild(div2);
 const parseData = JSON.parse(data);
 console.log(parseData);
 
-// parseData.data.forEach(element => {
-//     div2.insertAdjacentHTML("beforeend", `
-//         <figure>
-//         <img src="${element}" alt="dog" />
-//         <figcaption>Dog</figcaption>
-//         </figure>`)
-// });
+parseData.data.forEach(element => {
+    div2.insertAdjacentHTML("beforeend", `
+        <figure>
+        <img src="${element}" alt="dog" />
+        <figcaption>Dog</figcaption>
+        </figure>`)
+});
 
 const url = 'https://dattebayo-api.onrender.com/characters';
 async function getData(url) {
     const response = await fetch(url);
-    // json() - даст чисто данные json без лишней воды
     const json1 = await response.json();
     return json1;
 }
-// обработка ошибки
+
 try {
-    // ожидание работы асинхронной функции - await
     const myData = await getData(url);
     console.log(`${myData}`);
     myData.forEach(element => {
