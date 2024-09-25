32301bec01d3669cd95676d152280da2f541796d
reistorm <rysaeva96@mail.ru>
2024-09-25 12:50:21 +0500

    end

--- NodeJS/Homework/Homework1/homework1.js
+++ NodeJS/Homework/Homework1/homework1.js
@@ -10,26 +10,50 @@
 
 const http = require('http');
 
+let indexCount = 0;
+let aboutCount = 0;
+
+// Функция для создания HTML-страницы
+const createPage = (content, count) => `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page</title>
+    </head>
+    <body>
+        <h1>${content}</h1>
+        <p>Количество просмотров: ${count}</p>
+        <a href="/">На главную</a>
+    </body>
+    </html>
+`;
+
+
+
 const server = http.createServer((req, res) => {
     console.log('Запрос получен');
-    let count = 0;
+    
     if(req.url === '/') {
+        indexCount++;
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=UTF-8',
         });
-        res.end('<a href="./about">About</a>');
+        res.end(createPage('Главная страница', indexCount));
     } else if(req.url === '/about') {
+        aboutCount++;
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=UTF-8',
         });
-        res.end('<a href="#">Главная страница</a>');
+        res.end(createPage('About', aboutCount));
        
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html; charset=UTF-8',
         });
         
-        res.end('<a href="#""></a><h1>Страница не найдена</h1>');
+        res.end('<h1>Страница не найдена</h1>');
         
     }
     
